94e7aaa1553bac3dde0bfa3399058b687938ecd9
Ashwin Aadithan <ashwin21rr@gmail.com>
2022-07-07 10:39:15 +0530

    feat (ticket): added addImages and deleteImage

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Req } from '@nestjs/common';
+import { Controller, Post, Body, Req, UseInterceptors, Param, UploadedFiles, Delete } from '@nestjs/common';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { TicketService } from './ticket.service';
 import { Request } from 'express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('ticket')
 export class TicketController {
@@ -10,5 +11,16 @@ export class TicketController {
   @Post()
   create(@Body() createTicketDto: CreateTicketDto, @Req() req: Request) {
     return this.ticketService.create(createTicketDto, req['user']);
+  }
+
+  @Post(':id/image')
+  @UseInterceptors(FilesInterceptor('images'))
+  addImages(@Param('id') id: string, @UploadedFiles() images: Express.Multer.File[]) {
+    return this.ticketService.addImages(id, images);
+  }
+
+  @Delete(':id/image/:image')
+  deleteImage(@Param('id') id: string, @Param('image') image: string) {
+    return this.ticketService.deleteImage(id, image);
   }
 }
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -4,6 +4,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { TicketDocument } from './schema/ticket.schema';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { User } from './interface/user';
+import { S3 } from 'aws-sdk';
+import { randomBytes } from 'crypto';
 
 
 @Injectable()
@@ -13,5 +15,55 @@ export class TicketService {
     async create(createTicketDto: CreateTicketDto, user: User) {
         const ticket = new this.TicketModel({ ...createTicketDto, user_id: user._id });
         return await ticket.save();
+    }
+
+    async addImages(id: string, images: Express.Multer.File[]) {
+        const imageUrls = await this.uploadImages(images);
+        const ticket = await this.TicketModel.findById(id);
+
+        ticket.images.concat(imageUrls);
+        return await ticket.save();
+    }
+
+    async deleteImage(id: string, url: string) {
+        const ticket = await this.TicketModel.findById(id);
+
+        ticket.images = ticket.images.filter(image => image !== url);
+        return await ticket.save();
+    }
+
+    async uploadImages(images: Express.Multer.File[]) {
+        const imageUrls = [];
+        for (let image of images) {
+            const [type, ext] = image.mimetype.split('/');
+            if (type === 'image') {
+                const fileName = randomBytes(16).toString('hex') + '.' + ext;
+                const res = await this.uploadToS3(image.buffer, fileName);
+                imageUrls.push(res['Location']);
+            }
+        }
+
+        return imageUrls;
+    }
+
+    async uploadToS3(fileBuffer: Buffer, fileName: string) {
+        const s3 = new S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        });
+        const params: S3.PutObjectRequest = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Key: fileName,
+            Body: fileBuffer,
+        };
+
+        return new Promise((resolve, reject) => {
+            s3.upload(params, (err, data) => {
+                if (err) {
+                    reject(err.message);
+                }
+                resolve(data);
+            });
+        });
     }
 }
