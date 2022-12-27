43b9c09015079b328a799d7784634fb3e487b1cb
Khavin Shankar <khavinshankar@gmail.com>
2022-12-27 15:38:55 +0530

    removed enum validation for role in user

--- src/auth/schema/user.schema.ts
+++ src/auth/schema/user.schema.ts
@@ -8,33 +8,35 @@ export const ROLES = ["ADMIN", "CUSTOMER"]
 
 @Schema()
 export class User {
-    @Prop({ required: true })
-    full_name: string;
+  @Prop({ required: true })
+  full_name: string
 
-    @Prop({ required: true, unique: true, lowercase: true, index: true })
-    username: string;
+  @Prop({ required: true, unique: true, lowercase: true, index: true })
+  username: string
 
-    @Prop({ required: true, unique: true, lowercase: true, index: true })
-    email: string;
+  @Prop({ required: true, unique: true, lowercase: true, index: true })
+  email: string
 
-    @Prop({ required: true, uppercase: true, default: "customer", enum: ROLES })
-    role: string;
+  @Prop({ required: true, uppercase: true, default: 'customer' })
+  role: string
 
-    @Prop(raw({
-        address1: { type: String, required: true },
-        address2: { type: String, required: true },
-        landmark: { type: String },
-        city: { type: String, required: true },
-        pincode: { type: Number, required: true, min: 100000, max: 999999 },
-        state: { type: String, required: true },
-    }))
-    address: Record<string, any>;
+  @Prop(
+    raw({
+      address1: { type: String, required: true },
+      address2: { type: String, required: true },
+      landmark: { type: String },
+      city: { type: String, required: true },
+      pincode: { type: Number, required: true, min: 100000, max: 999999 },
+      state: { type: String, required: true },
+    }),
+  )
+  address: Record<string, any>
 
-    @Prop({ required: true })
-    password_hashed: string;
+  @Prop({ required: true })
+  password_hashed: string
 
-    @Prop()
-    token_hashed: string;
+  @Prop()
+  token_hashed: string
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
